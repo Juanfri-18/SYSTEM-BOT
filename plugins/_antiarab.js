12a2eb1782366c98bd4f357d1162b625197a5060
𝐒𝐘𝐒𝐓𝐄𝐌 𝐁𝐎𝐓 <benjaminexe2023@gmail.com>
2024-06-29 17:28:19 -1100

    Update _antiarab.js

--- plugins/_antiarab.js
+++ plugins/_antiarab.js
@@ -1,28 +1,13 @@
-const handler = (m) => m;
-handler.before = async function(m, {conn, isAdmin, isBotAdmin, isOwner, isROwner} ) {
-  /* if (m.message) {
-    console.log(m.message)
-  }*/
-  if (!m.isGroup) return !1;
+const handler = async (m, { conn, isAdmin, isBotAdmin, isOwner, isROwner }) => {
+  if (!m.isGroup) return false;
   const chat = global.db.data.chats[m.chat];
   const bot = global.db.data.settings[conn.user.jid] || {};
   if (isBotAdmin && chat.antiArab2 && !isAdmin && !isOwner && !isROwner && bot.restrict) {
-    if (m.sender.startsWith('212' || '212')) {
-      m.reply(`*[❗] ᴇɴ ᴇsᴛᴇ ɢʀᴜᴘᴏ ɴᴏ sᴇ ᴘᴇʀᴍɪᴛᴇɴ ɴᴜᴍᴇʀᴏs ᴀʀᴀʙᴇs ᴏ ʀᴀʀᴏs, ᴘᴏʀ ʟᴏ ϙᴜᴇ sᴇ ᴛᴇ sᴀᴄᴀʀᴀ ᴅᴇʟ ɢʀᴜᴘᴏ*`);
-      const responseb = await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove');
-      if (responseb[0].status === '404') return;
-    }
-
-    if (m.sender.startsWith('265' || '265')) {
-      m.reply(`*[❗] ᴇɴ ᴇsᴛᴇ ɢʀᴜᴘᴏ ɴᴏ sᴇ ᴘᴇʀᴍɪᴛᴇɴ ɴᴜᴍᴇʀᴏs ᴀʀᴀʙᴇs ᴏ ʀᴀʀᴏs, ᴘᴏʀ ʟᴏ ϙᴜᴇ sᴇ ᴛᴇ sᴀᴄᴀʀᴀ ᴅᴇʟ ɢʀᴜᴘᴏ*`);
-      const responseb = await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove');
-      if (responseb[0].status === '404') return;
-    }
-
-    if (m.sender.startsWith('92' || '92')) {
-      m.reply(`*[❗] ᴇɴ ᴇsᴛᴇ ɢʀᴜᴘᴏ ɴᴏ sᴇ ᴘᴇʀᴍɪᴛᴇɴ ɴᴜᴍᴇʀᴏs ᴀʀᴀʙᴇs ᴏ ʀᴀʀᴏs, ᴘᴏʀ ʟᴏ ϙᴜᴇ sᴇ ᴛᴇ sᴀᴄᴀʀᴀ ᴅᴇʟ ɢʀᴜᴘᴏ*`);
-      const responseb = await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove');
-      if (responseb[0].status === '404') return;
+    const arabicNumbers = ['212', '265', '92'];
+    if (arabicNumbers.some(prefix => m.sender.startsWith(prefix))) {
+      m.reply(`*[❗] En este grupo no se permiten números árabes o raros, por lo que se te sacará del grupo.*`);
+      const response = await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove');
+      if (response[0].status === '404') return;
     }
   }
 };
