6b509019342b0e5a63a3138fba9c02da42cfc026
𝐒𝐘𝐒𝐓𝐄𝐌 𝐁𝐎𝐓 <benjaminexe2023@gmail.com>
2024-06-29 17:57:56 -1100

    Update _antitoxic.js

--- plugins/_antitoxic.js
+++ plugins/_antitoxic.js
@@ -1,28 +1,23 @@
 const toxicRegex = /SJADJASDJASJGFHJDFKHGFDJKGDFHKGDaFJKHGDKAHSKIDHUITOAIDXFOKJNFVBNCM,VBNCV,MB6/i;
 
-export async function before(m, {isAdmin, isBotAdmin, isOwner}) {
-  if (m.isBaileys && m.fromMe) {
-    return !0;
-  }
-  if (!m.isGroup) {
-    return !1;
-  }
+export async function before(m, { isAdmin, isBotAdmin, isOwner }) {
+  if (m.isBaileys && m.fromMe) return true;
+  if (!m.isGroup) return false;
   const user = global.db.data.users[m.sender];
   const chat = global.db.data.chats[m.chat];
-  const bot = global.db.data.settings[mconn.conn.user.jid] || {};
   const isToxic = toxicRegex.exec(m.text);
 
   if (isToxic && chat.antiToxic && !isOwner && !isAdmin) {
     user.warn += 1;
-    if (!(user.warn >= 5)) await m.reply('*[❗] ' + `${user.warn == 1 ? `Hola @${m.sender.split`@`[0]}` : `@${m.sender.split`@`[0]}`}, decir la palabra "${isToxic}" está prohibido en este grupo. Advertencia: ${user.warn}/5.` + '*', false, {mentions: [m.sender]});
+    if (!(user.warn >= 5)) await m.reply(`*❱❱ 𝐑𝐄𝐆𝐋𝐀𝐒 𝐒𝐘𝐒𝐓𝐄𝐌 𝐁𝐎𝐓 ❰❰ ${user.warn == 1 ? `𝐒𝐀𝐋𝐔𝐃𝐎𝐒... @${m.sender.split`@`[0]}` : `@${m.sender.split`@`[0]}`}, 𝐌𝐄𝐍𝐂𝐈𝐎𝐍𝐀𝐑 𝐋𝐀 𝐏𝐀𝐋𝐀𝐁𝐑𝐀 "${isToxic}" 𝐄𝐒𝐓𝐀 𝐏𝐑𝐎𝐇𝐈𝐁𝐈𝐃𝐀 𝐔𝐒𝐀𝐑𝐋𝐀 𝐘𝐀 𝐐𝐔𝐄 𝐄𝐋 𝐂𝐎𝐌𝐀𝐍𝐃𝐎 𝐀𝐍𝐓𝐈𝐓𝐎𝐗𝐈𝐂 𝐄𝐒𝐓𝐀 𝐀𝐂𝐓𝐈𝐕𝐀𝐃𝐎. 𝐀𝐃𝐕𝐄𝐑𝐓𝐄𝐍𝐂𝐈𝐀: ${user.warn}/5.*`, false, { mentions: [m.sender] });
   }
 
   if (user.warn >= 5) {
     user.warn = 0;
-    await m.reply(`*[❗] Hola @${m.sender.split`@`[0]}, superaste las 5 advertencias por lo que serás eliminado de este grupo por tu comportamiento.*`, false, {mentions: [m.sender]});
+    await m.reply(`*❱❱ 𝐑𝐄𝐆𝐋𝐀𝐒 𝐒𝐘𝐒𝐓𝐄𝐌 𝐁𝐎𝐓 ❰❰ 𝐒𝐀𝐋𝐔𝐃𝐎𝐒... @${m.sender.split`@`[0]}, 𝐒𝐔𝐏𝐄𝐑𝐀𝐒𝐓𝐄 𝐄𝐋 𝐌𝐀𝐗𝐈𝐌𝐎 𝐃𝐄 𝐀𝐃𝐕𝐄𝐑𝐓𝐄𝐍𝐂𝐈𝐀𝐒, 𝐀𝐃𝐈𝐎𝐒.*`, false, { mentions: [m.sender] });
     user.banned = true;
     await mconn.conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove');
-    // await this.updateBlockStatus(m.sender, 'block')
   }
-  return !1;
+  return false;
 }
+export default before;
